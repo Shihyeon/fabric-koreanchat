b87c7c652f0c3ad41cbaa43415579c2bae70a4f3
Shihyeon <76618619+Shihyeon@users.noreply.github.com>
2024-06-20 04:23:16 +0900

    Add relative position indicator to the chat screen

--- src/main/java/com/hyfata/najoan/koreanpatch/mixin/indicator/ChatScreenMixin.java
+++ src/main/java/com/hyfata/najoan/koreanpatch/mixin/indicator/ChatScreenMixin.java
@@ -1,23 +1,37 @@
 package com.hyfata.najoan.koreanpatch.mixin.indicator;
 
 import com.hyfata.najoan.koreanpatch.util.Indicator;
+import com.hyfata.najoan.koreanpatch.util.TextFieldWidgetUtil;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ChatScreen;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value={ChatScreen.class})
 public abstract class ChatScreenMixin extends Screen {
+
+    @Shadow private TextFieldWidget chatField;
+
+    private TextFieldWidgetUtil textFieldWidgetUtil;
+    private int width;
+
     protected ChatScreenMixin(Text title) {
         super(title);
     }
 
     @Inject(at = {@At(value="HEAD")}, method = {"render"})
     private void addCustomLabel(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
-        Indicator.showIndicator(context, 2, this.height - 29, false);
+        textFieldWidgetUtil = new TextFieldWidgetUtil(chatField);
+        width = textFieldWidgetUtil.getTextWidth();
+        if (width >= client.getWindow().getWidth()) {
+            width = client.getWindow().getWidth();
+        }
+        Indicator.showIndicator(context, width + 2, this.height - 27, false);
     }
 }
