c832cca6d794275f0fd3a0b78672c345596a9ec8
Shihyeon <76618619+Shihyeon@users.noreply.github.com>
2024-06-22 02:06:46 +0900

    Fix alignment of text cursor and indicator x-position on the chat screen (#5)

---
+++ src/main/java/com/hyfata/najoan/koreanpatch/mixin/TextFieldWidgetAccessor.java
@@ -0,0 +1,14 @@
+package com.hyfata.najoan.koreanpatch.mixin;
+
+import net.minecraft.client.gui.widget.TextFieldWidget;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(TextFieldWidget.class)
+public interface TextFieldWidgetAccessor {
+    @Accessor("firstCharacterIndex")
+    int getFirstCharacterIndex();
+
+    @Accessor("selectionStart")
+    int getSelectionStart();
+}
