572873e0a609ee167b2e53ed16f7ec7aafb088d0
Najoan <68422289+najoan125@users.noreply.github.com>
2024-07-03 23:38:32 +0900

    fix warning

--- src/main/java/com/hyfata/najoan/koreanpatch/mixin/accessor/BookEditScreenPageContentAccessor.java
+++ src/main/java/com/hyfata/najoan/koreanpatch/mixin/accessor/BookEditScreenPageContentAccessor.java
@@ -4,7 +4,7 @@ import net.minecraft.client.gui.screen.ingame.BookEditScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-@Mixin(targets = "net.minecraft.client.gui.screen.ingame.BookEditScreen$PageContent")
+@Mixin(BookEditScreen.PageContent.class)
 public interface BookEditScreenPageContentAccessor {
     @Accessor("position")
     BookEditScreen.Position getPosition();
