89e4ba75eb89ba568f4ddbd9cba5f547f6ae0d0a
Najoan <68422289+najoan125@users.noreply.github.com>
2024-07-05 21:11:24 +0900

    fix indicator position in CreativeInventoryScreen

--- src/main/java/com/hyfata/najoan/koreanpatch/mixin/indicator/CreativeInventoryScreenMixin.java
+++ src/main/java/com/hyfata/najoan/koreanpatch/mixin/indicator/CreativeInventoryScreenMixin.java
@@ -4,9 +4,11 @@ import com.hyfata.najoan.koreanpatch.util.Indicator;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen;
+import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,6 +16,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = CreativeInventoryScreen.class)
 public class CreativeInventoryScreenMixin extends Screen {
+    @Shadow private TextFieldWidget searchBox;
+
     protected CreativeInventoryScreenMixin(Text title) {
         super(title);
     }
@@ -24,13 +28,10 @@ public class CreativeInventoryScreenMixin extends Screen {
     @Inject(method = {"render"}, at = @At(value = "TAIL", shift = At.Shift.BY, by = -3))
     private void addCustomLabel(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
         if (search) {
-            // init(): this.searchBox = new TextFieldWidget(this.textRenderer, this.x + 82, this.y + 6, 80
-            // HandledScreen.init(): this.x = (this.width - this.backgroundWidth) / 2
-            int x = (this.width - 176) / 2 + 82;
-            int y = (this.height - 166) / 2 + 6;
-            int searchBoxWidth = 80;
+            int x = searchBox.getX() + searchBox.getWidth() + 19;
+            int y = searchBox.getY() + searchBox.getHeight() / 2;
 
-            Indicator.showCenteredIndicator(context, x + searchBoxWidth + 8, y + 14);
+            Indicator.showCenteredIndicator(context, x, y);
         }
     }
 
