e6f9560be1be44eda893d0337f65cdd73885d105
Najoan <68422289+najoan125@users.noreply.github.com>
2024-07-20 14:21:49 +0900

    Clean up

--- src/main/java/com/hyfata/najoan/koreanpatch/mixin/SelectionManagerMixin.java
+++ src/main/java/com/hyfata/najoan/koreanpatch/mixin/SelectionManagerMixin.java
@@ -1,18 +1,13 @@
 package com.hyfata.najoan.koreanpatch.mixin;
 
-import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
-import com.hyfata.najoan.koreanpatch.keyboard.KeyboardLayout;
-import com.hyfata.najoan.koreanpatch.util.language.HangulProcessor;
-import com.hyfata.najoan.koreanpatch.util.language.HangulUtil;
-import com.hyfata.najoan.koreanpatch.util.language.LanguageUtil;
+import com.hyfata.najoan.koreanpatch.util.mixin.selectionmanager.ISelectionManagerAccessor;
+import com.hyfata.najoan.koreanpatch.util.mixin.selectionmanager.SelectionManagerHandler;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.util.InputUtil;
 import net.minecraft.client.util.SelectionManager;
-import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -22,8 +17,8 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(value={SelectionManager.class})
-public abstract class SelectionManagerMixin {
+@Mixin(value = {SelectionManager.class})
+public abstract class SelectionManagerMixin implements ISelectionManagerAccessor {
     @Shadow
     private int selectionEnd;
     @Shadow
@@ -36,248 +31,63 @@ public abstract class SelectionManagerMixin {
     @Final
     private Consumer<String> stringSetter;
 
-    @Shadow protected abstract String getSelectedText(String string);
+    @Shadow
+    protected abstract String getSelectedText(String string);
 
-    @Shadow protected abstract void insert(String string, String insertion);
+    @Shadow
+    protected abstract void insert(String string, String insertion);
 
     @Unique
-    private final MinecraftClient client = MinecraftClient.getInstance();
+    private final SelectionManagerHandler handler = new SelectionManagerHandler(this);
 
-    @Inject(at={@At(value="HEAD")}, method={"insert(C)Z"}, cancellable=true)
+    @Override
+    public int fabric_koreanchat$getCursor() {
+        return this.selectionEnd;
+    }
+
+    @Override
+    public Supplier<String> fabric_koreanchat$getStringGetter() {
+        return this.stringGetter;
+    }
+
+    @Override
+    public Predicate<String> fabric_koreanchat$getStringFilter() {
+        return this.stringFilter;
+    }
+
+    @Override
+    public Consumer<String> fabric_koreanchat$getStringSetter() {
+        return this.stringSetter;
+    }
+
+    @Override
+    public String fabric_koreanchat$selectedText(String string) {
+        return getSelectedText(string);
+    }
+
+    @Override
+    public void fabric_koreanchat$runInsert(String string, String insertion) {
+        insert(string, insertion);
+    }
+
+    @Inject(at = {@At(value = "HEAD")}, method = {"insert(C)Z"}, cancellable = true)
     public void insertChar(char chr, CallbackInfoReturnable<Boolean> cir) {
-        if (this.client.currentScreen != null && LanguageUtil.isKorean()) {
-            cir.setReturnValue(Boolean.TRUE);
-            if (chr == ' ') {
-                this.writeText(String.valueOf(chr));
-                KeyboardLayout.INSTANCE.assemblePosition = HangulProcessor.isHangulCharacter(chr) ? this.selectionEnd : -1;
-                return;
-            }
-            int qwertyIndex = KeyboardLayout.INSTANCE.getQwertyIndexCodePoint(chr);
-            if (qwertyIndex == -1) {
-                KeyboardLayout.INSTANCE.assemblePosition = -1;
-                return;
-            }
-            Objects.requireNonNull(KeyboardLayout.INSTANCE);
-            char curr = KeyboardLayout.INSTANCE.layout.toCharArray()[qwertyIndex];
-            int cursorPosition = this.selectionEnd;
-            int modifiers = this.getModifiers();
-            if (cursorPosition == 0 || !HangulProcessor.isHangulCharacter(curr) || !this.onHangulCharTyped(chr, modifiers)) {
-
-                this.writeText(String.valueOf(HangulUtil.getFixedHangulChar(modifiers, chr, curr)));
-                KeyboardLayout.INSTANCE.assemblePosition = HangulProcessor.isHangulCharacter(curr) ? this.selectionEnd : -1;
-            }
-        }
+        handler.insertChar(chr, cir);
     }
 
-    @Inject(at={@At(value="HEAD")}, method={"insert(Ljava/lang/String;)V"}, cancellable=true)
+    @Inject(at = {@At(value = "HEAD")}, method = {"insert(Ljava/lang/String;)V"}, cancellable = true)
     public void insertString(String string, CallbackInfo ci) {
-        for (char chr : string.toCharArray()) {
-            if (this.client.currentScreen == null || !LanguageUtil.isKorean()) continue;
-            ci.cancel();
-            if (chr == ' ') {
-                this.writeText(String.valueOf(chr));
-                KeyboardLayout.INSTANCE.assemblePosition = HangulProcessor.isHangulCharacter(chr) ? this.selectionEnd : -1;
-                continue;
-            }
-            if (chr == '\n') {
-                this.writeText(String.valueOf(chr));
-                KeyboardLayout.INSTANCE.assemblePosition = HangulProcessor.isHangulCharacter(chr) ? this.selectionEnd : -1;
-                continue;
-            }
-            int qwertyIndex = KeyboardLayout.INSTANCE.getQwertyIndexCodePoint(chr);
-            if (qwertyIndex == -1) {
-                KeyboardLayout.INSTANCE.assemblePosition = -1;
-                continue;
-            }
-            Objects.requireNonNull(KeyboardLayout.INSTANCE);
-            char curr = KeyboardLayout.INSTANCE.layout.toCharArray()[qwertyIndex];
-            int cursorPosition = this.selectionEnd;
-            int modifiers = this.getModifiers();
-            if (cursorPosition != 0 && HangulProcessor.isHangulCharacter(curr) && this.onHangulCharTyped(chr, modifiers)) continue;
-
-            this.writeText(String.valueOf(HangulUtil.getFixedHangulChar(modifiers, chr, curr)));
-            KeyboardLayout.INSTANCE.assemblePosition = HangulProcessor.isHangulCharacter(curr) ? this.selectionEnd : -1;
-        }
+        handler.insertString(string, ci);
     }
 
-    @Inject(at={@At(value = "HEAD")}, method = {"delete(I)V"}, cancellable=true)
+    @Inject(at = {@At(value = "HEAD")}, method = {"delete(I)V"}, cancellable = true)
     public void delete(int offset, CallbackInfo ci) {
         MinecraftClient client = MinecraftClient.getInstance();
         if (client.currentScreen != null) {
-            if (onBackspaceKeyPressed()) {
+            if (handler.onBackspaceKeyPressed()) {
                 ci.cancel();
             }
         }
-    }
-
-
-    @Unique
-    boolean onBackspaceKeyPressed() {
-        int cursorPosition = this.selectionEnd;
-        if (cursorPosition == 0 || cursorPosition != KeyboardLayout.INSTANCE.assemblePosition) return false;
-
-        String text = this.getText();
-
-        char ch = text.toCharArray()[cursorPosition - 1];
-
-        if (HangulProcessor.isHangulSyllables(ch)) {
-            int code = ch - 0xAC00;
-            int cho = code / (21 * 28);
-            int jung = (code % (21 * 28)) / 28;
-            int jong = (code % (21 * 28)) % 28;
-
-            char[] ch_arr;
-            if (jong != 0) {
-                ch_arr = KeyboardLayout.INSTANCE.jongsung_ref_table.get(jong).toCharArray();
-                if (ch_arr.length == 2) {
-                    jong = KeyboardLayout.INSTANCE.jongsung_table.indexOf(ch_arr[0]);
-                } else {
-                    jong = 0;
-                }
-                char c = HangulProcessor.synthesizeHangulCharacter(cho, jung, jong);
-                this.modifyText(c);
-            } else {
-                ch_arr = KeyboardLayout.INSTANCE.jungsung_ref_table.get(jung).toCharArray();
-                if (ch_arr.length == 2) {
-                    jung = KeyboardLayout.INSTANCE.jungsung_table.indexOf(ch_arr[0]);
-                    char c = HangulProcessor.synthesizeHangulCharacter(cho, jung, 0);
-                    this.modifyText(c);
-                } else {
-                    char c = KeyboardLayout.INSTANCE.chosung_table.charAt(cho);
-                    this.modifyText(c);
-                }
-            }
-            return true;
-        } else if (HangulProcessor.isHangulCharacter(ch)) {
-            KeyboardLayout.INSTANCE.assemblePosition = -1;
-            return false;
-        }
-        return false;
-    }
-
-    @Unique
-    private int getModifiers() {
-        boolean shift = InputUtil.isKeyPressed(client.getWindow().getHandle(), GLFW.GLFW_KEY_LEFT_SHIFT) ||
-                InputUtil.isKeyPressed(client.getWindow().getHandle(), GLFW.GLFW_KEY_RIGHT_SHIFT);
-        if (shift) {
-            return 1;
-        }
-        return 0;
-    }
-
-    @Unique
-    public String getText() {
-        return this.stringGetter.get();
-    }
-
-    @Unique
-    public void writeText(String str) {
-        insert(this.getText(), str);
-    }
-
-    @Unique
-    public void setText(String str) {
-        if (this.stringFilter.test(str)) {
-            this.stringSetter.accept(str);
-        }
-    }
-
-    @Unique
-    public void modifyText(char ch) {
-        int cursorPosition = this.selectionEnd;
-        char[] arr = this.getText().toCharArray();
-        if (cursorPosition > 0 && cursorPosition <= arr.length) {
-            arr[cursorPosition - 1] = ch;
-            this.setText(String.valueOf(arr));
-        }
-    }
-
-    @Unique
-    boolean onHangulCharTyped(int keyCode, int modifiers) {
-        int idx = HangulUtil.getFixedQwertyIndex(keyCode, modifiers);
-        // System.out.println(String.format("idx: %d", idx));
-        if (idx == -1) {
-            KeyboardLayout.INSTANCE.assemblePosition = -1;
-            return false;
-        }
-
-        int cursorPosition = this.selectionEnd;
-        String text = this.getText();
-
-        char prev = text.toCharArray()[cursorPosition - 1];
-        char curr = KeyboardLayout.INSTANCE.layout.toCharArray()[idx];
-
-        if (cursorPosition == KeyboardLayout.INSTANCE.assemblePosition && getSelectedText(stringGetter.get()).isEmpty()) {
-            // 자음 + 모음
-            if (HangulProcessor.isJaeum(prev) && HangulProcessor.isMoeum(curr)) {
-                int cho = KeyboardLayout.INSTANCE.chosung_table.indexOf(prev);
-                int jung = KeyboardLayout.INSTANCE.jungsung_table.indexOf(curr);
-                char c = HangulProcessor.synthesizeHangulCharacter(cho, jung, 0);
-                this.modifyText(c);
-                KeyboardLayout.INSTANCE.assemblePosition = this.selectionEnd;
-                return true;
-            }
-
-            if (HangulProcessor.isHangulSyllables(prev)) {
-                int code = prev - 0xAC00;
-                int cho = code / (21 * 28);
-                int jung = (code % (21 * 28)) / 28;
-                int jong = (code % (21 * 28)) % 28;
-
-                // 중성 합성 (ㅘ, ㅙ)..
-                if (jong == 0 && HangulProcessor.isJungsung(prev, curr)) {
-                    jung = HangulProcessor.getJungsung(prev, curr);
-                    char c = HangulProcessor.synthesizeHangulCharacter(cho, jung, 0);
-                    this.modifyText(c);
-                    KeyboardLayout.INSTANCE.assemblePosition = this.selectionEnd;
-                    return true;
-                }
-
-                // 종성 추가
-                if (jong == 0 && HangulProcessor.isJongsung(curr)) {
-                    char c = HangulProcessor.synthesizeHangulCharacter(cho, jung, HangulProcessor.getJongsung(curr));
-                    this.modifyText(c);
-                    KeyboardLayout.INSTANCE.assemblePosition = this.selectionEnd;
-                    return true;
-                }
-
-                // 종성 받침 추가
-                if (jong != 0 && HangulProcessor.isJongsung(prev, curr)) {
-                    jong = HangulProcessor.getJongsung(prev, curr);
-                    char c = HangulProcessor.synthesizeHangulCharacter(cho, jung, jong);
-                    this.modifyText(c);
-                    KeyboardLayout.INSTANCE.assemblePosition = this.selectionEnd;
-                    return true;
-                }
-
-                // 종성에서 받침 하나 빼고 글자 만들기
-                if (jong != 0 && HangulProcessor.isJungsung(curr)) {
-                    char[] tbl = KeyboardLayout.INSTANCE.jongsung_ref_table.get(jong).toCharArray();
-                    int newCho;
-                    if (tbl.length == 2) {
-                        newCho = KeyboardLayout.INSTANCE.chosung_table.indexOf(tbl[1]);
-                        jong = KeyboardLayout.INSTANCE.jongsung_table.indexOf(tbl[0]);
-                    } else {
-                        newCho = KeyboardLayout.INSTANCE.chosung_table.indexOf(KeyboardLayout.INSTANCE.jongsung_table.charAt(jong));
-                        jong = 0;
-                    }
-
-                    char c = HangulProcessor.synthesizeHangulCharacter(cho, jung, jong);
-                    this.modifyText(c);
-
-                    cho = newCho;
-                    jung = KeyboardLayout.INSTANCE.jungsung_table.indexOf(curr);
-                    code = HangulProcessor.synthesizeHangulCharacter(cho, jung, 0);
-                    this.writeText(String.valueOf(Character.toChars(code)));
-                    KeyboardLayout.INSTANCE.assemblePosition = this.selectionEnd;
-                    return true;
-                }
-            }
-        }
-
-        this.writeText(String.valueOf(curr));
-        KeyboardLayout.INSTANCE.assemblePosition = this.selectionEnd;
-        return true;
     }
 }
 
--- src/main/java/com/hyfata/najoan/koreanpatch/mixin/TextFieldWidgetMixin.java
+++ src/main/java/com/hyfata/najoan/koreanpatch/mixin/TextFieldWidgetMixin.java
@@ -1,13 +1,10 @@
 package com.hyfata.najoan.koreanpatch.mixin;
 
 import com.hyfata.najoan.koreanpatch.client.KoreanPatchClient;
-import com.hyfata.najoan.koreanpatch.keyboard.KeyboardLayout;
-import com.hyfata.najoan.koreanpatch.mixin.accessor.CreativeInventoryScreenInvoker;
-import com.hyfata.najoan.koreanpatch.util.language.HangulProcessor;
-import com.hyfata.najoan.koreanpatch.util.language.HangulUtil;
 import com.hyfata.najoan.koreanpatch.util.language.LanguageUtil;
+import com.hyfata.najoan.koreanpatch.util.mixin.textfieldwidget.ITextFieldWidgetAccessor;
+import com.hyfata.najoan.koreanpatch.util.mixin.textfieldwidget.TextFieldWidgetHandler;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen;
 import net.minecraft.client.gui.screen.ingame.JigsawBlockScreen;
 import net.minecraft.client.gui.screen.ingame.StructureBlockScreen;
 import net.minecraft.client.gui.screen.world.CreateWorldScreen;
@@ -23,7 +20,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import java.util.function.Consumer;
 
 @Mixin(value = {TextFieldWidget.class})
-public abstract class TextFieldWidgetMixin {
+public abstract class TextFieldWidgetMixin implements ITextFieldWidgetAccessor {
     @Shadow
     private Consumer<String> changedListener;
     @Unique
@@ -59,198 +56,18 @@ public abstract class TextFieldWidgetMixin {
     @Shadow
     public abstract String getSelectedText();
 
-    @Unique
-    public void writeText(String str) {
-        this.write(str);
-        this.sendTextChanged(str);
-        this.onChanged(this.getText());
-        this.updateScreen();
+    @Override
+    public Consumer<String> fabric_koreanchat$getChangedListener() {
+        return this.changedListener;
+    }
+
+    @Override
+    public void fabric_koreanchat$changed(String var1) {
+        onChanged(var1);
     }
 
     @Unique
-    private void sendTextChanged(String str) {
-        if (this.changedListener != null) {
-            this.changedListener.accept(str);
-        }
-    }
-
-    @Unique
-    private void updateScreen() {
-        if (this.client.currentScreen == null) {
-            return;
-        }
-        if (this.client.currentScreen instanceof CreativeInventoryScreen && !this.getText().isEmpty()) {
-            ((CreativeInventoryScreenInvoker) this.client.currentScreen).updateCreativeSearch();
-        }
-    }
-
-    @Unique
-    public void modifyText(char ch) {
-        int cursorPosition = this.getCursor();
-        this.setCursor(cursorPosition - 1, false);
-        this.eraseCharacters(1);
-        this.writeText(String.valueOf(Character.toChars(ch)));
-    }
-
-    @Unique
-    boolean onBackspaceKeyPressed() {
-        if (!getSelectedText().isEmpty()) {
-            return false;
-        }
-
-        int cursorPosition = this.getCursor();
-        if (cursorPosition == 0 || cursorPosition != KeyboardLayout.INSTANCE.assemblePosition) return false;
-
-        String text = this.getText();
-
-        char ch = text.toCharArray()[cursorPosition - 1];
-
-        if (HangulProcessor.isHangulSyllables(ch)) {
-            int code = ch - 0xAC00;
-            int cho = code / (21 * 28);
-            int jung = (code % (21 * 28)) / 28;
-            int jong = (code % (21 * 28)) % 28;
-
-            char[] ch_arr;
-            if (jong != 0) {
-                ch_arr = KeyboardLayout.INSTANCE.jongsung_ref_table.get(jong).toCharArray();
-                if (ch_arr.length == 2) {
-                    jong = KeyboardLayout.INSTANCE.jongsung_table.indexOf(ch_arr[0]);
-                } else {
-                    jong = 0;
-                }
-                char c = HangulProcessor.synthesizeHangulCharacter(cho, jung, jong);
-                this.modifyText(c);
-            } else {
-                ch_arr = KeyboardLayout.INSTANCE.jungsung_ref_table.get(jung).toCharArray();
-                if (ch_arr.length == 2) {
-                    jung = KeyboardLayout.INSTANCE.jungsung_table.indexOf(ch_arr[0]);
-                    char c = HangulProcessor.synthesizeHangulCharacter(cho, jung, 0);
-                    this.modifyText(c);
-                } else {
-                    char c = KeyboardLayout.INSTANCE.chosung_table.charAt(cho);
-                    this.modifyText(c);
-                }
-            }
-            return true;
-        } else if (HangulProcessor.isHangulCharacter(ch)) {
-            KeyboardLayout.INSTANCE.assemblePosition = -1;
-            return false;
-        }
-        return false;
-    }
-
-    @Unique
-    boolean onHangulCharTyped(int keyCode, int modifiers) {
-        int idx = HangulUtil.getFixedQwertyIndex(keyCode, modifiers);
-        // System.out.println(String.format("idx: %d", idx));
-        if (idx == -1) {
-            KeyboardLayout.INSTANCE.assemblePosition = -1;
-            return false;
-        }
-
-        int cursorPosition = this.getCursor();
-        String text = this.getText();
-
-        char prev = text.toCharArray()[cursorPosition - 1];
-        char curr = KeyboardLayout.INSTANCE.layout.toCharArray()[idx];
-
-        if (cursorPosition == KeyboardLayout.INSTANCE.assemblePosition && getSelectedText().isEmpty()) {
-
-            // 자음 + 모음
-            if (HangulProcessor.isJaeum(prev) && HangulProcessor.isMoeum(curr)) {
-                int cho = KeyboardLayout.INSTANCE.chosung_table.indexOf(prev);
-                int jung = KeyboardLayout.INSTANCE.jungsung_table.indexOf(curr);
-                char c = HangulProcessor.synthesizeHangulCharacter(cho, jung, 0);
-                this.modifyText(c);
-                KeyboardLayout.INSTANCE.assemblePosition = this.getCursor();
-                return true;
-            }
-
-            if (HangulProcessor.isHangulSyllables(prev)) {
-                int code = prev - 0xAC00;
-                int cho = code / (21 * 28);
-                int jung = (code % (21 * 28)) / 28;
-                int jong = (code % (21 * 28)) % 28;
-
-                // 중성 합성 (ㅘ, ㅙ)..
-                if (jong == 0 && HangulProcessor.isJungsung(prev, curr)) {
-                    jung = HangulProcessor.getJungsung(prev, curr);
-                    char c = HangulProcessor.synthesizeHangulCharacter(cho, jung, 0);
-                    this.modifyText(c);
-                    KeyboardLayout.INSTANCE.assemblePosition = this.getCursor();
-                    return true;
-                }
-
-                // 종성 추가
-                if (jong == 0 && HangulProcessor.isJongsung(curr)) {
-                    char c = HangulProcessor.synthesizeHangulCharacter(cho, jung, HangulProcessor.getJongsung(curr));
-                    this.modifyText(c);
-                    KeyboardLayout.INSTANCE.assemblePosition = this.getCursor();
-                    return true;
-                }
-
-                // 종성 받침 추가
-                if (jong != 0 && HangulProcessor.isJongsung(prev, curr)) {
-                    jong = HangulProcessor.getJongsung(prev, curr);
-                    char c = HangulProcessor.synthesizeHangulCharacter(cho, jung, jong);
-                    this.modifyText(c);
-                    KeyboardLayout.INSTANCE.assemblePosition = this.getCursor();
-                    return true;
-                }
-
-                // 종성에서 받침 하나 빼고 글자 만들기
-                if (jong != 0 && HangulProcessor.isJungsung(curr)) {
-                    char[] tbl = KeyboardLayout.INSTANCE.jongsung_ref_table.get(jong).toCharArray();
-                    int newCho;
-                    if (tbl.length == 2) {
-                        newCho = KeyboardLayout.INSTANCE.chosung_table.indexOf(tbl[1]);
-                        jong = KeyboardLayout.INSTANCE.jongsung_table.indexOf(tbl[0]);
-                    } else {
-                        newCho = KeyboardLayout.INSTANCE.chosung_table.indexOf(KeyboardLayout.INSTANCE.jongsung_table.charAt(jong));
-                        jong = 0;
-                    }
-
-                    char c = HangulProcessor.synthesizeHangulCharacter(cho, jung, jong);
-                    this.modifyText(c);
-
-                    cho = newCho;
-                    jung = KeyboardLayout.INSTANCE.jungsung_table.indexOf(curr);
-                    code = HangulProcessor.synthesizeHangulCharacter(cho, jung, 0);
-                    this.writeText(String.valueOf(Character.toChars(code)));
-                    KeyboardLayout.INSTANCE.assemblePosition = this.getCursor();
-                    return true;
-                }
-            }
-        }
-
-        this.writeText(String.valueOf(curr));
-        KeyboardLayout.INSTANCE.assemblePosition = this.getCursor();
-        return true;
-    }
-
-    @Unique
-    public void typedTextField(char chr, int modifiers, CallbackInfoReturnable<Boolean> cir) {
-        int qwertyIndex = KeyboardLayout.INSTANCE.getQwertyIndexCodePoint(chr);
-        if (qwertyIndex == -1) {
-            KeyboardLayout.INSTANCE.assemblePosition = -1;
-            return;
-        }
-
-        if (this.isActive()) {
-            cir.setReturnValue(Boolean.TRUE);
-        } else {
-            cir.setReturnValue(Boolean.FALSE);
-            return;
-        }
-
-        char curr = KeyboardLayout.INSTANCE.layout.toCharArray()[qwertyIndex];
-        if (this.getCursor() == 0 || !HangulProcessor.isHangulCharacter(curr) || !onHangulCharTyped(chr, modifiers)) {
-
-            this.writeText(String.valueOf(HangulUtil.getFixedHangulChar(modifiers, chr, curr)));
-            KeyboardLayout.INSTANCE.assemblePosition = HangulProcessor.isHangulCharacter((curr)) ? this.getCursor() : -1;
-        }
-    }
+    private final TextFieldWidgetHandler handler = new TextFieldWidgetHandler(this);
 
     @Inject(at = {@At(value = "HEAD")}, method = {"charTyped(CI)Z"}, cancellable = true)
     public void charTyped(char chr, int modifiers, CallbackInfoReturnable<Boolean> cir) {
@@ -259,7 +76,7 @@ public abstract class TextFieldWidgetMixin {
                 !(this.client.currentScreen instanceof StructureBlockScreen) &&
                 !(this.client.currentScreen instanceof CreateWorldScreen && !KoreanPatchClient.gameTab) &&
                 LanguageUtil.isKorean() && this.isEditable() && Character.charCount(chr) == 1) {
-            typedTextField(chr, modifiers, cir);
+            handler.typedTextField(chr, modifiers, cir);
         }
     }
 
@@ -274,7 +91,7 @@ public abstract class TextFieldWidgetMixin {
                 LanguageUtil.toggleCurrentType();
             }
             if (keyCode == GLFW.GLFW_KEY_BACKSPACE) {
-                if (onBackspaceKeyPressed()) {
+                if (handler.onBackspaceKeyPressed()) {
                     callbackInfo.setReturnValue(Boolean.TRUE);
                 }
             }
