6e05f370d1e6cc9470e1df3933de261a04ab1129
Najoan <68422289+najoan125@users.noreply.github.com>
2024-07-24 00:39:13 +0900

    bug fix #18

---
+++ src/main/java/com/hyfata/najoan/koreanpatch/mixin/KeyboardMixin.java
@@ -0,0 +1,34 @@
+package com.hyfata.najoan.koreanpatch.mixin;
+
+import com.hyfata.najoan.koreanpatch.client.KoreanPatchClient;
+import com.hyfata.najoan.koreanpatch.util.ModLogger;
+import com.hyfata.najoan.koreanpatch.util.language.LanguageUtil;
+import net.minecraft.client.Keyboard;
+import net.minecraft.client.MinecraftClient;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Keyboard.class)
+public class KeyboardMixin {
+    @Shadow
+    @Final
+    private MinecraftClient client;
+
+    @Inject(method = "onKey", at = @At("HEAD"))
+    private void onInput(long window, int keyCode, int scanCode, int action, int modifiers, CallbackInfo ci) {
+        if (window != client.getWindow().getHandle() ||
+                action == 0 ||
+                client.currentScreen == null) return;
+        ModLogger.debug(keyCode + " " + scanCode + " " + modifiers + " " + action);
+
+        if (KoreanPatchClient.imeBinding.matchesKey(keyCode, scanCode) && modifiers == 2) {
+            KoreanPatchClient.getController().toggleFocus();
+        } else if (KoreanPatchClient.langBinding.matchesKey(keyCode, scanCode) && !KoreanPatchClient.IME) {
+            LanguageUtil.toggleCurrentType();
+        }
+    }
+}
--- src/main/java/com/hyfata/najoan/koreanpatch/mixin/indicator/AnvilScreenMixin.java
+++ src/main/java/com/hyfata/najoan/koreanpatch/mixin/indicator/AnvilScreenMixin.java
@@ -1,9 +1,6 @@
 package com.hyfata.najoan.koreanpatch.mixin.indicator;
 
-import com.hyfata.najoan.koreanpatch.client.KoreanPatchClient;
 import com.hyfata.najoan.koreanpatch.util.Indicator;
-import com.hyfata.najoan.koreanpatch.util.ModLogger;
-import com.hyfata.najoan.koreanpatch.util.language.LanguageUtil;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.ingame.AnvilScreen;
@@ -14,7 +11,6 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = {AnvilScreen.class})
 public class AnvilScreenMixin extends Screen {
@@ -32,15 +28,5 @@ public class AnvilScreenMixin extends Screen {
         float y = nameField.getY() - Indicator.getIndicatorHeight() - 6;
 
         Indicator.showIndicator(context, x, y);
-    }
-
-    @Inject(method = {"keyPressed"}, at = @At(value = "HEAD"))
-    private void keyPress(int keyCode, int scanCode, int modifiers, CallbackInfoReturnable<Boolean> cir) {
-        ModLogger.debug(keyCode + " " + scanCode + " " + modifiers);
-        if (KoreanPatchClient.imeBinding.matchesKey(keyCode, scanCode) && modifiers == 2) {
-            KoreanPatchClient.getController().toggleFocus();
-        } else if (KoreanPatchClient.langBinding.matchesKey(keyCode, scanCode) && !KoreanPatchClient.IME) {
-            LanguageUtil.toggleCurrentType();
-        }
     }
 }
